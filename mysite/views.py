f34d0e051853cc684bd8eef68f103353ea086098
Manuel <m.munoz.sanchez@student.tue.nl>
2017-11-01 21:23:25 +0100

    Added (sketchy) success message

--- mysite/views.py
+++ mysite/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
+# TODO: Proper django form
 def update_profile(request):
     if request.method == 'POST':
-        user_id = int(request.user.id)
-        user = User.objects.get(id=user_id)
-        email = request.POST['email']
-        user.username = email
-        user.email = email
-        user.save()
-    return redirect('/activities')
+    	if request.user.is_authenticated():
+	        user = User.objects.get(id=int(request.user.id))
+	        email = request.POST['email']
+	        user.username = email
+	        user.email = email
+	        user.save()
+	        return redirect('/profile?updated=true')
+    return redirect('/login')
