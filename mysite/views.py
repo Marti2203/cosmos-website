361afdd6dbe7e052f1f812a1fa94e0d0476ab9b5
Manuel <m.munoz.sanchez@student.tue.nl>
2017-11-01 21:22:58 +0100

    retrieving user id from user object instead of post params

--- mysite/views.py
+++ mysite/views.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 
 def update_profile(request):
     if request.method == 'POST':
-        user_id = int(request.POST['id'])
+        user_id = int(request.user.id)
         user = User.objects.get(id=user_id)
         email = request.POST['email']
+        user.username = email
         user.email = email
         user.save()
     return redirect('/activities')
