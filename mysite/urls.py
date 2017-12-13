5f877cdaa3f0269a1f177ce8e759170fcb5ed7a4
Niels de Jong <niels@nielsdejong.nl>
2017-12-13 14:42:26 +0100

    Added password reset functionality

--- mysite/urls.py
+++ mysite/urls.py
@@ -30,6 +30,10 @@ urlpatterns = patterns('',
   url(r'^requests/$', views.list_requests, name='list_requests'),
   url(r'^accept-request/$', views.accept_request, name='accept_request'),
   url(r'^reject-request/$', views.reject_request, name='reject_request'),
+  url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+  url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+  url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,  name='password_reset_confirm'),
+  url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
   url(r'^', include('cms.urls')),
 )
 
