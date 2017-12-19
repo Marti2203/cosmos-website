f1c199d2edbf69e05fdac94cbed9925f2b9099e2
root <root@cosmos>
2017-12-19 15:07:22 +0000

    Updated gitignore, production settings file and wsgi script

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+path = '/home/cosmos/web/cosmoswebsite'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings_pr")
 
 application = get_wsgi_application()
