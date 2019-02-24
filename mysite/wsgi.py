60c7f1047bd553f201779514095c541a632477f1
root <root@cosmostue.nl>
2019-02-24 22:32:06 +0000

    Updated wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,22 +1,22 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-
-"""
 
 import os
 import sys
+import site
 
 from django.core.wsgi import get_wsgi_application
 
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/cosmos/web/cosmoswebsite/env_cosmosweb_36/lib/python3.6/site-packages')
+
+# Add the app's directory to the PYTHONPATH
 path = '/home/cosmos/web/cosmoswebsite'
 if path not in sys.path:
     sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings_pr")
+
+# Activate virtual env
+activate_env=os.path.expanduser("/home/cosmos/web/cosmoswebsite/env_cosmosweb_36/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 application = get_wsgi_application()
