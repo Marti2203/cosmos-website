840a965faffb75533dca05e26667750ba7c7cd3b
Martin Mirchev <mirchevmartin2203@gmail.com>
2020-07-04 14:23:23 +0200

    Added administration routes

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}}),
-    # TODO idfk
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^\.well-known/', include('letsencrypt.urls')),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}),
